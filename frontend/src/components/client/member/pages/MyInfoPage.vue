fb813681f74e26a6778fea9913d70911023a1b92
Horingring <dldldksl@naver.com>
2022-02-22 00:12:43 +0900

    (Front) [Update] 회원정보관리페이지 정보변경모드 화면적용

--- frontend/src/components/client/member/pages/MyInfoPage.vue
+++ frontend/src/components/client/member/pages/MyInfoPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="myInfoPage">
+  <div id="myInfoPage" :class="{ 'on-auth': onAuth }">
     <div class="content_wrap">
       <h2>회원정보관리</h2>
 
@@ -41,9 +41,13 @@
                     v-model="userInfo.nick"
                     placeholder="닉네임을 입력해 주세요."
                   />
-                  <div>
-                    <button onClick="onClickUpdateImgBtn">변경</button>
-                    <button onClick="onClickDeleteImgBtn">삭제</button>
+                  <div class="btn_wrap">
+                    <button @click="onClickUpdateImgBtn" class="change">
+                      변경
+                    </button>
+                    <button @click="onClickDeleteImgBtn" class="delete">
+                      삭제
+                    </button>
                   </div>
                 </div>
               </div>
@@ -55,7 +59,11 @@
               '변경' 버튼 클릭시 인증 후 변경 가능
             </td>
             <td v-else>
-              <input type="password" v-model="userInfo.pw" />
+              <input
+                type="password"
+                v-model="userInfo.pw"
+                placeholder="새로운 비밀번호를 입력해 주세요."
+              />
             </td>
           </tr>
           <tr v-if="onAuth">
@@ -78,15 +86,19 @@
           </tr>
           <tr>
             <th scope="row">성별</th>
-            <td>{{ userInfo.sex }}</td>
+            <td>{{ userInfo.sex === "male" ? "남" : "여" }}</td>
           </tr>
         </tbody>
       </table>
 
-      <div class="btn_wrap" :class="{ 'on-auth': onAuth }">
-        <button v-if="!onAuth">회원정보 변경</button>
-        <button v-else>적용</button>
-        <button v-if="onAuth">취소</button>
+      <div class="btn_wrap">
+        <button v-if="!onAuth" @click="onClickChangeInfoBtn">
+          회원정보 변경
+        </button>
+        <button v-else class="apply" @click="onClickApplyBtn">적용</button>
+        <button v-if="onAuth" class="cancel" @click="onClickCancelBtn">
+          취소
+        </button>
       </div>
     </div>
   </div>
@@ -110,6 +122,8 @@ export default {
         birth: undefined,
         sex: undefined,
       }, // 회원 상세정보
+
+      tmpUserInfo: {},
     };
   },
   computed: {
@@ -117,6 +131,8 @@ export default {
   },
 
   created() {
+    this.onAuth = false;
+
     this.go();
   },
 
@@ -138,8 +154,46 @@ export default {
         eMail: "sonny_7@gmail.com",
         phone: "010-1234-5678",
         birth: "19920708",
-        sex: "mail",
+        sex: "male",
       };
+
+      // deep copy
+      this.tmpUserInfo = JSON.parse(JSON.stringify(this.userInfo));
+    },
+
+    /**
+     * 이미지 변경버튼 클릭시
+     */
+    onClickUpdateImgBtn() {
+      console.log("이미지 변경 btn 클릭");
+    },
+    /**
+     * 이미지 삭제버튼 클릭시
+     */
+    onClickDeleteImgBtn() {
+      console.log("이미지 삭제 btn 클릭");
+    },
+
+    /**
+     * 회원정보 변경버튼 클릭시
+     */
+    onClickChangeInfoBtn() {
+      this.onAuth = true;
+    },
+
+    /**
+     * 적용버튼 클릭시
+     */
+    onClickApplyBtn() {
+      console.log("적용 btn 클릭");
+    },
+    /**
+     * 취소버튼 클릭시
+     */
+    onClickCancelBtn() {
+      this.onAuth = false;
+      this.userInfo = this.tmpUserInfo;
+      scrollTo(0, 0);
     },
   },
 };
